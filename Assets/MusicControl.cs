c58f9c92d679a87707c28018415a5d5d320bbd1f
Demián <demiandal@gmail.com>
2016-04-01 19:58:04 -0500

    dfffs

---
+++ Assets/MusicControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using FMODUnity;
+public class MusicControl : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Q)){
+			FMODUnity.RuntimeManager.PlayOneShot("event:/Music/HellCat");
+
+
+		}
+		if(Input.GetKeyDown(KeyCode.W)){
+			FMODUnity.RuntimeManager.PlayOneShot("event:/Music/Music");
+				
+		}
+		if(Input.GetKeyDown(KeyCode.E)){
+			FMODUnity.RuntimeManager.PlayOneShot("event:/Ambience/Country");
+
+
+		}
+		if(Input.GetKeyDown(KeyCode.R)){
+			FMODUnity.RuntimeManager.PlayOneShot("event:/Explosions/Single Explosion");
+
+		}
+	}
+}
